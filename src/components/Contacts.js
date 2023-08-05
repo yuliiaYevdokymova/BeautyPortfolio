2eb928dc8a9dda563c9651306136eac41d7e9b63
Dmytro Porokhovnik <DPorokhovnik@wscad.com>
2023-08-05 16:15:37 +0200

    contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { Box, Grid, Typography, Container } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  IconButton,
+} from "@mui/material";
+import TikTokIcon from "./TikTokIcon";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { useTheme } from "@mui/material/styles";
 
 const Contacts = () => {
   const { t } = useTranslation();
+  const theme = useTheme();
+  const test = theme.palette.background.default;
 
   return (
     <Grid
-      container     
+      container
       direction="column"
       justify="center"
       textAlign="center"
@@ -17,9 +27,39 @@ const Contacts = () => {
         {t("Contacts")}
       </Typography>
 
+      <Box>
+        <Typography justify="center" sx={{ marginTop: 2 }}>
+          {t("LocationTitle")} : {t("Location")}
+        </Typography>
+
+        <Typography justify="center" sx={{ marginTop: 2 }}>
+          {t("Phone")} : +4917682277511
+        </Typography>
+
+        <Typography justify="center" sx={{ marginTop: 2 }}>
+          {t("Email")} : yuliya.yevdokimova@gmail.com
+        </Typography>
+      </Box>
+
       <Typography justify="center" sx={{ marginTop: 2 }} variant="h5">
         {t("FollowMe")}
       </Typography>
+      <Box>
+        <IconButton
+          sx={{ width: "40px" }}
+          href="https://instagram.com/yuliiamore?igshid=MzRlODBiNWFlZA=="
+          target="_blank"
+        >
+          <InstagramIcon />
+        </IconButton>
+        <IconButton
+          sx={{ width: "40px" }}
+          href="https://www.tiktok.com/@yuliiamore?_t=8eSKhziobH3&_r=1"
+          target="_blank"
+        >
+          <TikTokIcon/>
+        </IconButton>
+      </Box>
     </Grid>
   );
 };
